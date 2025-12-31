0970a3a0c6016e6e3f765d55d7c8fdb7d2f8da79
netschlurf <netschulurf@gmail.com>
2025-12-31 11:40:40 +0100

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,37 @@
 module.exports = {
   apps: [
     {
-      name: 'CT-SUIUSDT-LONG', // Name des zweiten Prozesses
-      script: './bot/MakerOne.js',
-      args: 'sui.json',
+      name: "maker-btc",
+      script: "bot/MakerOne.js",
+      args: "btc.json",
+      interpreter: "node",
+      autorestart: true,
       watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
     },
     {
-      name: 'CT-SUIUSDT-SHORT', // Name des zweiten Prozesses
-      script: './bot/MakerOne.js',
-      args: 'suishort.json',
+      name: "maker-sol",
+      script: "bot/MakerOne.js",
+      args: "sol.json",
+      interpreter: "node",
+      autorestart: true,
       watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
     },
     {
-      name: 'CT-EasyServer', // Name des zweiten Prozesses
-      script: './bot/EasyServer.js',
+      name: "maker-eth",
+      script: "bot/MakerOne.js",
+      args: "eth.json",
+      interpreter: "node",
+      autorestart: true,
       watch: false,
-    },
-    {
-      name: 'CT-PosWatch', // Name des zweiten Prozesses
-      script: './bot/PosWatch.js',
-      args: 'sui.json',
-      watch: false,
-    },
-
-
-  ],
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
 };
-
